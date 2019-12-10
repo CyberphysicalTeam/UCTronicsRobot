fba49cf10247e3468a112e57dfadfa5e854178e0
Robert Darrow <32168203+rwdarrow@users.noreply.github.com>
2019-12-10 00:43:22 -0500

    Add files via upload

--- wifi_client.ino
+++ wifi_client.ino
@@ -4,83 +4,81 @@
 #define OUT_PIN 5
 
 // set the IP address of the server
-IPAddress server(192,168,1,55);
+// SET TO IP ADDRESS GIVEN TO SERVER BY ROUTER
+IPAddress server(0, 0, 0, 0);
 WiFiClient client;
 
-const char* ssid = "ORBI85";           
-const char* password = "Retired2018!"; 
-
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
 bool detected = false;
 
 void setup() {
   pinMode(IN_PIN, INPUT);
   pinMode(OUT_PIN, OUTPUT);
-  
+
   Serial.begin(115200);
-  WiFi.begin(ssid, password);             
+  WiFi.begin(ssid, password);
   Serial.print("Connecting to ");
   Serial.print(ssid); Serial.println(" ...");
 
   int i = 0;
   // Wait for the Wi-Fi to connect
-  while (WiFi.status() != WL_CONNECTED) { 
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(++i); Serial.print(' ');
   }
 
+  // display connection details
   Serial.println('\n');
-  Serial.println("Connection established!");  
+  Serial.println("Connection established!");
   Serial.print("IP address:\t");
-  // Send the IP address of the ESP8266 to the computer
   Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // connect to server on port 80
-  if (client.connect(server, 80)) {
-    if (!detected) {
-      Serial.println("Connected to master robot");
-      detected = true;
-    }
-
-    // get command from server
-    int command = client.read();
-    
-    // send command to Arduino received from server
-    if (command > 0) {
-      Serial.print("Command received: ");
-      Serial.println(command);
-    
-      switch(command) {
-        case 1:
-          Serial.println("Forward");
-          digitalWrite(OUT_PIN, 1);
-          break;
-        case 2:
-          Serial.println("Backward");
-          digitalWrite(OUT_PIN, 2);
-          break;
-        case 3:
-          Serial.println("Left");
-          digitalWrite(OUT_PIN, 3);
-          break;
-        case 4:
-          Serial.println("Right");
-          digitalWrite(OUT_PIN, 4);
-          break;
-        case 5:
-          Serial.println("Stop");
-          digitalWrite(OUT_PIN, 5);
-          break;
-        case 6:
-          Serial.println("No command");
-        default:
-          break;
-      }
+  // connect to server
+  if (!client.connected()) {
+    if (client.connect(server, 80)) {
+      Serial.println("Connected to slave robot");
     } else {
-      Serial.println("No command received");
+      Serial.println("Slave robot not detected");
     }
-  } else {
-    Serial.println("Master robot not detected");
+  }
+
+  while (client.connected()) {
+    // get movement command from Arduino
+    int command = digitalRead(IN_PIN);
+    int data;
+
+    // send command to client (other bot)
+    Serial.print("Command sent: ");
+    switch (command) {
+      case 1:
+        Serial.println("Forward");
+        data = 1;
+        break;
+      case 2:
+        Serial.println("Backward");
+        data = 2;
+        break;
+      case 3:
+        Serial.println("Left");
+        data = 3;
+        break;
+      case 4:
+        Serial.println("Right");
+        data = 4;
+        break;
+      case 5:
+        Serial.println("Stop");
+        data = 5;
+        break;
+      default:
+        Serial.println("No command");
+        data = 6;
+        break;
+    }
+    // send command to server
+    client.write(data);
   }
 }
--- wifi_server.ino
+++ wifi_server.ino
@@ -4,87 +4,110 @@
 #define OUT_PIN 5
 
 // create a webserver listening on port 80
-WiFiServer server(80); 
+WiFiServer server(80);
 
-const char* ssid = "ORBI85";           
-const char* password = "Retired2018!";     
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+bool alreadyConnected = false;
+bool notConnected = true;
+bool notAvailable = true;
 
 void setup() {
   pinMode(IN_PIN, INPUT);
   pinMode(OUT_PIN, OUTPUT);
 
-  // Start the Serial communication to send messages to the computer
-  Serial.begin(115200);         
-  delay(10);
+  // Start serial communication 
+  Serial.begin(115200);
+  delay(1000);
   Serial.println('\n');
 
   // Connect to the network
-  WiFi.begin(ssid, password);             
+  WiFi.begin(ssid, password);
   Serial.print("Connecting to ");
   Serial.print(ssid); Serial.println(" ...");
 
   int i = 0;
   // Wait for the Wi-Fi to connect
-  while (WiFi.status() != WL_CONNECTED) { 
+  while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(++i); Serial.print(' ');
   }
 
   Serial.println('\n');
-  Serial.println("Connection established!");  
+  Serial.println("Connection established!");
   Serial.print("IP address:\t");
   // Send the IP address of the ESP8266 to the computer
-  Serial.println(WiFi.localIP());         
+  Serial.println(WiFi.localIP());
 
   // start the server
   server.begin();
+  server.setNoDelay(true);
 }
 
-void loop() { 
+void loop() {
   WiFiClient client = server.available();
 
   // continuously send movement commands to client
   if (client) {
     if (client.connected()) {
-      // get movement command from Arduino
-      int command = digitalRead(IN_PIN);
-      int data;
-      
-      // send command to client (other bot)
-      Serial.print("Command sent: ");
-      switch(command) {
-        case 1:
-          Serial.println("Forward");
-          data = 1;
-          break;
-        case 2: 
-          Serial.println("Backward");
-          data = 2;
-          break;
-        case 3:
-          Serial.println("Left");
-          data = 3;
-          break;
-        case 4:
-          Serial.println("Right");
-          data = 4;
-          break;
-        case 5:
-          Serial.println("Stop");
-          data = 5;
-          break;
-        default:
-          Serial.println("No command");
-          data = 6;
-          break;
+      if (!alreadyConnected) {
+        Serial.println("Master robot connected");
+        alreadyConnected = true;
       }
-      // send command
-      server.write(data);
-      
-      // flush command sent to client
-      client.flush();
+
+      while (!client.available()) {}
+
+      if (client.available() > 0) {
+        // read data from client
+        int command = client.read();
+
+        if (command != -1) {
+          Serial.print("Command received: ");
+
+          switch (command) {
+            case 1:
+              Serial.println("Forward");
+              digitalWrite(OUT_PIN, 1);
+              break;
+            case 2:
+              Serial.println("Backward");
+              digitalWrite(OUT_PIN, 2);
+              break;
+            case 3:
+              Serial.println("Left");
+              digitalWrite(OUT_PIN, 3);
+              break;
+            case 4:
+              Serial.println("Right");
+              digitalWrite(OUT_PIN, 4);
+              break;
+            case 5:
+              Serial.println("Stop");
+              digitalWrite(OUT_PIN, 5);
+              break;
+            case 6:
+              Serial.println("No command");
+            default:
+              break;
+          }
+        } else {
+          Serial.println("No command received");
+        }
+      } else {
+        alreadyConnected = false;
+        client.stop();
+      }
+    } else {
+      if (notConnected) {
+        Serial.println("No client connected");
+        notConnected = false;
+      }
     }
-    // terminate connection with client
-    client.stop();
+  } else {
+    if (notAvailable) {
+      Serial.println("No client available");
+      notAvailable = false;
+    }
   }
 }
